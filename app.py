167b201be8cce7bca244e29d9edd3a3eb913fe46
brittwitham <brittany.witham@gmail.com>
2022-07-26 10:26:35 -0400

    Updates site title

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(
     __name__, 
-    title='Don et Benevolat',
+    title='Centre Canadien de Connaissances sur les Dons et le Bénévolat',
     suppress_callback_exceptions=True, 
     external_stylesheets=[dbc.themes.BOOTSTRAP]
 )

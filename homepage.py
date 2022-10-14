1ba97c6fd22d36d918dd06aa63330adabd6e87f2
sakifff <34672660+sakifff@users.noreply.github.com>
2022-10-14 13:32:32 -0700

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -12,7 +12,7 @@ navbar = dbc.NavbarSimple(
                 dbc.NavLink("EN", href="http://app.givingandvolunteering.ca/",external_link=True)
             ),
         ],
-        brand="TEST Centre Canadien de Connaissances sur les Dons et le Bénévolat",
+        brand="Centre Canadien de Connaissances sur les Dons et le Bénévolat",
         brand_href="/",
         color="#4B161D",
         dark=True,

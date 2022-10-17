543a3770efffefd44d7a679c9fcf35a51b72cdfb
brittwitham <brittany.witham@gmail.com>
2022-10-17 18:08:25 -0400

    Adds phase 2 apps

---
+++ apps/qui_sont_les_benevoles_et_combien_dheures_donnent_ils_2013.py
@@ -0,0 +1,147 @@
+import dash
+from dash import dcc, html
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+import dash_bootstrap_components as dbc
+import os
+import os.path as op
+
+from utils.graphs.WTO0207_graph_utils import rate_avg_cause, single_vertical_percentage_graph
+from utils.data.WTO0207_data_utils import get_data, process_data, get_region_names, get_region_values
+
+from app import app
+from homepage import footer #navbar, footer
+
+####################### Data processing ######################
+BarriersVol_2018, SubSecAvgHrs_2018, SubSecVolRates_2018, AllocationVol_2018 = get_data()
+
+data = [SubSecVolRates_2018, SubSecAvgHrs_2018, AllocationVol_2018]
+process_data(data)
+
+# region_values = get_region_values()
+# region_names = get_region_names()
+region_names = ['CA', 'BC', 'AB', 'PR', 'ON', 'QC', 'AT']
+###################### App layout ######################
+navbar = dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(
+                # dcc.Link("Home", href="/")
+                dbc.NavLink("À propos", href="https://www.donetbenevolat.ca/",external_link=True)
+            ),
+            dbc.NavItem(
+                dbc.NavLink("EN", href="http://app.givingandvolunteering.ca/What_types_of_organizations_do_Canadians_volunteer_for_2018",external_link=True)
+            ),
+        ],
+        brand="Centre Canadien de Connaissances sur les Dons et le Bénévolat",
+        brand_href="/",
+        color="#4B161D",
+        dark=True,
+        sticky='top'
+    )
+
+marginTop = 20
+
+layout = html.Div([
+    navbar,
+    html.Header([
+        html.Div(className='overlay'),
+        dbc.Container(
+            dbc.Row(
+                html.Div(
+                    html.Div([
+                        html.H1('qui sont les bénévoles et combien d’heures donnent-ils - 2013'),
+                        ],
+                        className='post-heading'
+                    ),
+                    className='col-md-10 col-lg-8 mx-auto position-relative'
+                )
+            )
+        ),
+    ],
+        # className='masthead'
+        className="bg-secondary text-white text-center py-4",
+    ),
+    # Dropdown menu
+    dbc.Container([
+        dbc.Row(
+           dbc.Col(
+               html.Div([
+                   "Sélectionnez une région:",
+                   dcc.Dropdown(
+                       id='region-selection',
+                       options=[{'label': region_names[i], 'value': region_names[i]} for i in range(len(region_names))],
+                       value='CA',
+                       ),
+                    html.Br(),
+                ],className="m-2 p-2"),
+            ),id='sticky-dropdown'),
+    ],className='sticky-top bg-light mb-2', fluid=True),
+   dbc.Container(
+       dbc.Row([
+            html.Div(
+                [
+                    html.P('''
+                    (Paragraph)
+                    ''',className='mt-4'),
+                    html.P('''
+                    (Paragraph)
+                    '''),
+                    html.P('''
+                    (Paragraph)
+                    '''),
+                    # Levels of support by cause
+                    dcc.Graph(id='DonRateAvgDonAmt-Cause-2', style={'marginTop': marginTop}),
+                    html.Br(),
+                    dcc.Markdown('''
+                    (Paragraph)
+                    '''),
+                    # Allocation of support by cause
+                    dcc.Graph(id='AllocationSupport-Cause-2', style={'marginTop': marginTop}),
+                ], className='col-md-10 col-lg-8 mx-auto'
+            ),
+        ]),
+   ),
+   footer
+])
+
+################## CALLBACKS ##################
+
+# @app.callback(
+
+#     dash.dependencies.Output('DonRateAvgDonAmt-Cause-2', 'figure'),
+#     [
+#         dash.dependencies.Input('region-selection', 'value')
+#     ])
+# def update_graph(region):
+
+#     dff1 = SubSecVolRates_2018[SubSecVolRates_2018['Region'] == region]
+#     dff1 = dff1[dff1['Group'] == "All"]
+#     dff1 = dff1.replace("Volunteer rate", "Taux de bénévolat")
+#     # name1 = "Volunteer rate"
+#     name1 = "Taux de bénévolat"
+
+#     dff2 = SubSecAvgHrs_2018[SubSecAvgHrs_2018['Region'] == region]
+#     dff2 = dff2[dff2['Group'] == "All"]
+#     dff2 = dff2.replace("Average hours", "Nombre d'heures moyen")
+#     # name2 = "Average hours"
+#     name2 = "Nombre d'heures moyen"
+
+#     title = '{}, {}'.format("Niveaux de soutien par cause", region)
+
+#     return rate_avg_cause(dff1, dff2, name1, name2, title, vol=True)
+
+
+# @app.callback(
+
+#     dash.dependencies.Output('AllocationSupport-Cause-2', 'figure'),
+#     [
+#         dash.dependencies.Input('region-selection', 'value')
+#     ])
+# def update_graph(region):
+
+#     dff = AllocationVol_2018[AllocationVol_2018['Region'] == region]
+#     title = '{}, {}'.format("Répartition du soutien par cause", region)
+
+#     return single_vertical_percentage_graph(dff, title, by="QuestionText", sort=True)

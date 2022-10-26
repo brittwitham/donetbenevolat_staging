90923060a0f773347a8372aac13f087aa8c7e35e
chrisrtl <115814255+chrisrtl@users.noreply.github.com>
2022-10-26 12:22:31 -0400

    Update dons_et_benevolat_pour_les_organismes_du_secteur_de_leducation_2018.py

--- apps/dons_et_benevolat_pour_les_organismes_du_secteur_de_leducation_2018.py
+++ apps/dons_et_benevolat_pour_les_organismes_du_secteur_de_leducation_2018.py
@@ -105,20 +105,20 @@ layout = html.Div([
                 ], className='col-md-10 col-lg-8 mx-auto'
             ),
             html.Div([
-                    html.H3('Qui donne de l’argent'),
+                    html.H4('Qui donne de l’argent'),
                     dcc.Markdown("""
                     Certaines personnes sont plus enclines que d’autres à donner aux organismes du secteur de l’éducation. À l’échelle nationale, la probabilité de donner augmente avec le niveau d’études et le revenu du ménage. Quant à l’âge, la probabilité de donner augmente fortement jusqu’à l’âge de 35 à 44 ans, puis décline légèrement. Les autres groupes qui se distinguent en étant plus enclins à donner, sont les femmes, les personnes mariées ou en union de fait, les personnes qui occupent un emploi et les personnes nées au Canada. 
                     """
                     ),
                     # Donation rate by key demographic characteristics graph
                     # dcc.Graph(id='DonRateAvgDonAmt-prv', figure=don_rate_avg_don_amt_prv(DonRates_2018, AvgTotDon_2018), style={'marginTop': 20}),
-                    html.H3('Méthodes de dons'),
+                    html.H4('Méthodes de dons'),
                     dcc.Markdown("""
                     On a demandé aux répondant.e.s à l’Enquête si l’un ou plusieurs de 13 types de sollicitations différents les conduisaient à donner. Bien que l’Enquête ne lie pas directement ces méthodes aux causes soutenues, la comparaison entre les donateur.trice.s au bénéfice des organismes du secteur de l’éducation et les autres (c.-à-d. les personnes qui ne soutenaient que d’autres causes) permet de comprendre comment les personnes ont tendance à soutenir financièrement cette catégorie d’organismes. À l’échelle nationale, les donateur.trice.s du secteur de l’éducation ont particulièrement tendance à donner en parrainant quelqu’un (par exemple, lors d’un événement), en réponse à une sollicitation au porte-à-porte, à leur lieu de travail, en assistant à un événement de bienfaisance ou en mémoire de quelqu’un. Ces personnes ne sont pas particulièrement plus susceptibles de donner dans un lieu de culte, en réponse à un appel à la télévision ou la radio ou par d’autres méthodes non mentionnées expressément dans le questionnaire de l’Enquête.
                     """),
                     #Donation rate by method graph
                     # dcc.Graph(id='DonRateAvgDonAmt-prv', figure=don_rate_avg_don_amt_prv(DonRates_2018, AvgTotDon_2018), style={'marginTop': 20}),
-                    html.H3('Motivations des dons'),
+                    html.H4('Motivations des dons'),
                     dcc.Markdown("""
                     On a demandé aux répondant.e.s à l’Enquête si huit facteurs potentiels jouaient un rôle important dans leurs décisions de donner. Là encore, bien qu’il n’existe aucun lien direct entre les motivations et les causes soutenues, la comparaison des personnes qui donnent aux organismes du secteur de l’éducation et de celles qui donnent aux autres organismes permet de comprendre les raisons de leur soutien de cette catégorie d’organismes. À l’échelle nationale, les donateur.trice.s aux organismes du secteur de l’éducation sont plus susceptibles que les personnes qui donnent aux autres organismes de contribuer financièrement à la suite de leur sollicitation par une personne de leur connaissance, parce que la cause de l’organisme les touche personnellement ou parce qu’elles connaissent une personne dans ce cas, pour apporter une contribution à la collectivité et parce qu’elles recevront un crédit d’impôt pour leur don. Les motivations religieuses et spirituelles ne semblent pas jouer un rôle particulièrement significatif dans les décisions de donner aux organismes du secteur de l’éducation. 
                     """),
@@ -129,7 +129,7 @@ layout = html.Div([
             # Barriers to donating
             html.Div(
                 [
-                    html.H3('Freins aux dons'),
+                    html.H4('Freins aux dons'),
                     html.P("""
                     Afin de mieux comprendre les facteurs qui peuvent dissuader de donner, on a demandé aux donateur.trice.s si dix freins potentiels les empêchent de donner plus. À l’échelle nationale, les personnes qui donnent aux organismes du secteur de l’éducation ont plus tendance que les autres donateur.trice.s à limiter leurs dons parce qu’elles n’aiment pas les méthodes de sollicitation et parce qu’elles croient avoir déjà donné assez. Les autres freins ont grosso modo une incidence similaire sur les personnes qui donnent aux organismes d’éducation et sur celles qui donnent aux autres types d’organismes.
                     """
@@ -140,7 +140,7 @@ layout = html.Div([
                 
                     # Volunteering levels
                     html.Div([
-                        html.H5("Niveaux de bénévolat"),
+                        html.H4("Niveaux de bénévolat"),
                         html.P("""
                         À l’échelle nationale, environ une personne sur 11 (9 %) au Canada a fait du bénévolat pour un organisme d’éducation et de recherche pendant l’année qui a précédé l’Enquête, ce qui fait de de la base de bénévoles de l’éducation la quatrième par ordre d’importance. Sur le plan des deux sous-causes, les personnes sont nettement moins enclines à faire du bénévolat pour les universités et les collèges que pour autres types d’organismes d’éducation et de recherche. Quant au nombre d’heures de bénévolat par cause, les organismes d’éducation et de recherche reçoivent un peu plus de 9 % du nombre total d’heures de bénévolat, à raison d’un tiers des heures pour les universités et les collèges et de deux tiers des heures pour les autres types d’organismes d’éducation et de recherche. À l’échelle nationale, les organismes du secteur de l’éducation et de la recherche se classent au quatrième rang du point de vue de la proportion du nombre total d’heures de bénévolat, derrière les organismes des arts et loisirs (22 %), les organismes des services sociaux (18 %) et les organismes religieux (16 %).
                         """),
@@ -149,7 +149,7 @@ layout = html.Div([
                     ]),
                     # Who volunteers
                     html.Div([
-                        html.H5("Qui fait du bénévolat"),
+                        html.H4("Qui fait du bénévolat"),
                         html.P("""
                         À l’échelle nationale, les groupes les plus enclins à faire du bénévolat pour les organismes du secteur de l’éducation sont les femmes, les personnes célibataires et qui ne se sont jamais mariées et celles titulaires soit d’un diplôme universitaire, soit titulaires ou non d’un diplôme d’études secondaires. Quant à l’effet de l’âge, la probabilité de faire du bénévolat est la plus élevée à l’âge de 15 à 24 ans, chute de 25 à 34 ans, culmine de 35 à 44 ans, puis baisse, ce qui correspond vraisemblablement aux différents stades de la vie. Le bénévolat a également tendance à augmenter avec le revenu du ménage, bien que les ménages au revenu inférieur à 20 000 $ s’écartent légèrement de cette tendance, au niveau national du moins.
                         """),
@@ -158,7 +158,7 @@ layout = html.Div([
                     ]),
                     # Volunteer activities
                     html.Div([
-                        html.H5("Activités des bénévoles"),
+                        html.H4("Activités des bénévoles"),
                         html.P("""
                         On a demandé aux personnes si, parmi 14 types d’activité différents, elles participaient à 1 ou plusieurs d’entre elles pour un organisme. Bien que l’Enquête ne lie pas précisément les activités aux types d’organismes soutenus, la comparaison des bénévoles des organismes du secteur de l’éducation et de la recherche et des bénévoles des autres organismes permet de comprendre les activités des bénévoles font pour cette catégorie d’organismes. Comme on pouvait s’y attendre, les bénévoles des organismes d’éducation et de recherche ont relativement tendance à enseigner et à servir de mentors, mais aussi à organiser des activités et des événements, à entraîner et à arbitrer dans le cadre sportif et à collecter des fonds. Ces bénévoles sont relativement peu susceptibles de fournir des soins de santé ou du soutien dans ce domaine ou de participer à des activités bénévoles non mentionnées expressément dans le questionnaire. 
                         """),
@@ -167,7 +167,7 @@ layout = html.Div([
                     ]),
                     # Motivations for volunteering
                     html.Div([
-                        html.H5("Motivations du bénévolat"),
+                        html.H4("Motivations du bénévolat"),
                         html.P("""
                         On a demandé aux répondant.e.s si douze facteurs potentiels jouaient un rôle important dans leur décision de faire don de leur temps. Contrairement à de nombreux autres domaines de l’Enquête, ces motivations sont liées précisément au bénévolat au bénéfice de causes particulières. À l’échelle nationale, les personnes qui font du bénévolat pour le secteur de l’éducation sont plus enclines à chercher à améliorer leurs possibilités d’emploi et à réseauter ou à rencontrer des personnes. Elles ont moins tendance à être motivées par des croyances religieuses ou spirituelles, parce qu’un membre de leur famille fait du bénévolat ou par le soutien d’une forme ou d’une autre de cause politique, environnementale ou sociale. 
                         """),
@@ -176,7 +176,7 @@ layout = html.Div([
                     ]),
                     # Barriers to volunteering
                     html.Div([
-                        html.H5("Freins au bénévolat"),
+                        html.H4("Freins au bénévolat"),
                         html.P("""
                         On a demandé aux bénévoles si douze freins potentiels les avaient empêchés de faire don de plus de temps pendant l’année précédente. Bien que les freins ne soient pas liés directement aux causes soutenues, la comparaison des bénévoles des organismes d’éducation et de recherche et de ceux des autres organismes apporte une information importante sur les facteurs pouvant importer particulièrement aux bénévoles de cette catégorie d’organismes. À l’échelle nationale, les bénévoles du secteur de l’éducation ont plus tendance à limiter leur bénévolat par manque de temps. Ces personnes ont légèrement moins tendance à penser avoir déjà fait don d’assez de temps ou à limiter leur bénévolat en raison de problèmes de santé ou d’obstacles physiques. 
                         """),

6454b54b1c03f8f7d571bcfc91ebf8a032283039
chrisrtl <115814255+chrisrtl@users.noreply.github.com>
2022-10-27 20:39:44 -0400

    Update les_dons_et_le_benevolat_des_jeunes_2018.py

--- apps/les_dons_et_le_benevolat_des_jeunes_2018.py
+++ apps/les_dons_et_le_benevolat_des_jeunes_2018.py
@@ -120,7 +120,7 @@ navbar = dbc.NavbarSimple(
                 dbc.NavLink("À propos", href="https://www.donetbenevolat.ca/",external_link=True)
             ),
             dbc.NavItem(
-                dbc.NavLink("EN", href="http://app.givingandvolunteering.ca/giving_and_volunteering_by_youth_2018",external_link=True)
+                dbc.NavLink("EN", href="http://app.givingandvolunteering.ca/giving_and_volunteering_by_youth",external_link=True)
             ),
         ],
         brand="Centre Canadien de Connaissances sur les Dons et le Bénévolat",

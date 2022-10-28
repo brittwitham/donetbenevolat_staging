c142e4d99711d2409b52d649d339949e7de76889
brittwitham <brittany.witham@gmail.com>
2022-10-28 18:26:07 -0400

    Reroutes what keeps canadians 2013 link

---
+++ utils/gen_navbar.py
@@ -0,0 +1,22 @@
+import dash_bootstrap_components as dbc
+
+
+def gen_navbar(alt_lang_link):
+    navbar = dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(
+                dbc.NavLink("About", href="https://www.donetbenevolat.ca/", external_link=True)
+            ),
+            dbc.NavItem(
+                dbc.NavLink("FR", href="http://app.givingandvolunteering.ca/{}".format(alt_lang_link), external_link=True)
+            ),
+
+        ],
+        brand="Canadian Knowledge Hub for Giving and Volunteering",
+        brand_href="/",
+        color="#4B161D",  # c7102e
+        dark=True,
+        sticky='top'
+    )
+
+    return navbar

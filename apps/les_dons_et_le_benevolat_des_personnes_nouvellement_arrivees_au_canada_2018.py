6280bfe906b649df7cea3b0a7351c000a1728b23
chrisrtl <115814255+chrisrtl@users.noreply.github.com>
2022-10-27 20:42:38 -0400

    Update les_dons_et_le_benevolat_des_personnes_nouvellement_arrivees_au_canada_2018.py

--- apps/les_dons_et_le_benevolat_des_personnes_nouvellement_arrivees_au_canada_2018.py
+++ apps/les_dons_et_le_benevolat_des_personnes_nouvellement_arrivees_au_canada_2018.py
@@ -116,7 +116,7 @@ navbar = dbc.NavbarSimple(
                 dbc.NavLink("À propos", href="https://www.donetbenevolat.ca/",external_link=True)
             ),
             dbc.NavItem(
-                dbc.NavLink("EN", href="http://app.givingandvolunteering.ca/What_types_of_organizations_do_Canadians_volunteer_for_2018",external_link=True)
+                dbc.NavLink("EN", href="http://app.givingandvolunteering.ca/giving_and_volunteering_among_new_canadians",external_link=True)
             ),
         ],
         brand="Centre Canadien de Connaissances sur les Dons et le Bénévolat",

75620abae113dbe86fa26527395295464a619d5f
Patrick Dorosz <patrick.dorosz@gmail.com>
2023-04-10 10:29:23 -0400

    Changes home button address

--- utils/home_button.py
+++ utils/home_button.py
@@ -4,10 +4,8 @@ import dash_bootstrap_components as dbc
 
 def gen_home_button(is_french = True, is_2013 = False):
     button_text = "Retour à la page d'accueil" if is_french else "Back to Home"
-     # TODO: Reenable french site when it's live
-    # button_link = "https://www.donetbenevolat.ca/" if is_french else "https://www.givingandvolunteering.ca/gssgvp-insights"
-    button_link="https://www.givingandvolunteering.ca/gssgvp-insights"
-
+    button_link = "https://www.donetbenevolat.ca/donnees-enquete-sociale-generale" if is_french else "https://www.givingandvolunteering.ca/gssgvp-insights"
+  
    
     button_link_with_year = button_link + "-2013" if is_2013 else button_link
 
